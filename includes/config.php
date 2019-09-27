cec24a79695b82aae1d6530d783b87f999d4126c
pebblestad <33155006+pebblestad@users.noreply.github.com>
2019-09-27 01:38:47 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,23 @@
-
+<?php
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Simple PHP Website',
+        'pretty_uri' => false,
+        'site_url' => 'https://csuphpdemo.herokuapp.com',
+        'nav_menu' => [
+            '' => 'Home',
+            'about-us' => 'About Us',
+            'contact' => 'Contact'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v3.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
