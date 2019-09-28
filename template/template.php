4c39c05ecef0c4a2b6b1aa233ce98cee280f964f
pebblestad <stadtmueller.johnathan@yahoo.com>
2019-09-28 22:13:10 +0000

    added more bootstrap

--- template/template.php
+++ template/template.php
@@ -12,6 +12,13 @@
     <title>Hello, world!</title>
   </head>
   <body>
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to the random comic generator</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
     <h1><?php site_name(); ?></h1>
 
     <div class="wrap">
