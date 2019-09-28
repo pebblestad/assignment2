ee5b01bcf232a05d941837cada738b93bf6fb615
pebblestad <stadtmueller.johnathan@yahoo.com>
2019-09-28 23:07:57 +0000

    added ajax

--- template/template.php
+++ template/template.php
@@ -49,6 +49,25 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+      $('#testButton').click(function (e){
+          $.ajax({
+              type: 'post',
+              url: "/includes/handleRand.php",
+              data: {'random' : true},
+              dataType:"HTML", // May be HTML or JSON as your wish
+              success: function(data)
+              {
+                  $('div #target_div').html(data) // The server's response is now placed inside your target div
+              },
+              error: function()
+              {
+                  alert("Failed to get data.");
+            }
+          }); // Ajax close
+          return false; // So the button click does not refresh the page
+      }) // Function end
+    </script>
   </body>
 </html>
 
