4f8f2bfc4b04ba09864307114c1c94a491536209
pebblestad <stadtmueller.johnathan@yahoo.com>
2019-09-28 22:15:35 +0000

    modifying bootstrap

--- template/template.php
+++ template/template.php
@@ -14,9 +14,9 @@
   <body>
     <div class="jumbotron">
         <h1 class="display-4">Welcome to the random comic generator</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p class="lead">Your number one source for comics!!</p>
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p>Click the button below to choose another comic!</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
     <h1><?php site_name(); ?></h1>
